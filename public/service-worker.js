b2c0703ee521502fa90a0a7f0de863b9c82f2f53
omERdem555 <omererdem555@outlook.com>
2026-05-19 12:49:34 +0300

    Chrome içinden gelen URL bildirmi engellenmiş olmalı

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v2';
+const CACHE_VERSION = 'v3';
 const RUNTIME_CACHE = `habit-tracker-runtime-${CACHE_VERSION}`;
 
 const PRECACHE_URLS = ['/', '/index.html', '/manifest.json', '/icon.png'];
